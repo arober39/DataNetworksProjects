97abd5462c781b6d9702b2cab433127b6ec43367
Alexis Roberson <alexisroberson@alexisroberson-macbookpro.roam.corp.google.com>
2019-03-27 22:51:28 -0700

    Add existing file

---
+++ proxy.py
@@ -0,0 +1,117 @@
+"""A proxy server that forwards requests from one port to another server.
+
+To run this using Python 2.7:
+
+% python proxy.py
+
+It listens on a port (`LISTENING_PORT`, below) and forwards commands to the
+server. The server is at `SERVER_ADDRESS`:`SERVER_PORT` below.
+"""
+
+# This code uses Python 2.7. These imports make the 2.7 code feel a lot closer
+# to Python 3. (They're also good changes to the language!)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import library
+import socket
+
+# Where to find the server. This assumes it's running on the smae machine
+# as the proxy, but on a different port.
+SERVER_ADDRESS = 'localhost'
+SERVER_PORT = 7777
+
+# The port that the proxy server is going to occupy. This could be the same
+# as SERVER_PORT, but then you couldn't run the proxy and the server on the
+# same machine.
+LISTENING_PORT = 8888
+
+# Cache values retrieved from the server for this long.
+MAX_CACHE_AGE_SEC = 60.0  # 1 minute
+
+
+def ForwardCommandToServer(command, server_addr, server_port):
+  """Opens a TCP socket to the server, sends a command, and returns response.
+
+  Args:
+    command: A single line string command with no newlines in it.
+    server_addr: A string with the name of the server to forward requests to.
+    server_port: An int from 0 to 2^16 with the port the server is listening on.
+  Returns:
+    A single line string response with no newlines.
+
+ 
+  """
+
+  ###################################################
+  #TODO: Implement Function: WiP
+  ###################################################
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((server_addr, server_port))
+  s.send(command)
+  response = library.ReadCommand(s)
+  return response
+ 
+  
+
+
+
+def CheckCachedResponse(command_line, cache, server_addr, server_port):
+
+  # Update the cache for PUT commands but also pass the traffic to the server.
+  ##########################
+  #TODO: Implement section
+  ##########################
+
+  # GET commands can be cached.
+
+  ############################
+  #TODO: Implement section
+  ############################
+  cmd, name, text = library.ParseCommand(command_line)
+
+  if cmd == "GET":
+    if cache.GetValue(name, MAX_CACHE_AGE_SEC):
+      return cache.GetValue(name, MAX_CACHE_AGE_SEC)
+    else:
+      #if not in cache
+      get = ForwardCommandToServer(command_line, server_addr, server_port)
+      cache.StoreValue(name, get) #store in cache
+      return get
+  elif cmd == "PUT":
+    if cache.GetValue(name, MAX_CACHE_AGE_SEC):
+      cache.StoreValue(name, text)
+    return ForwardCommandToServer(command_line, server_addr, server_port)
+  else:
+    return ForwardCommandToServer(command_line, server_addr, server_port)
+
+  
+def ProxyClientCommand(sock, server_addr, server_port, cache):
+
+  command_line = library.ReadCommand(sock)
+  response = CheckCachedResponse(command_line, cache, server_addr, server_port)
+  sock.send(response)
+
+
+
+def main():
+  # Listen on a specified port...
+  server_sock = library.CreateServerSocket(LISTENING_PORT)
+  cache = library.KeyValueStore()
+  # Accept incoming commands indefinitely.
+  while True:
+    # Wait until a client connects and then get a socket that connects to the
+    # client.
+    client_sock, (address, port) = library.ConnectClientToServer(server_sock)
+    print('Received connection from %s:%d' % (address, port))
+    ProxyClientCommand(client_sock, SERVER_ADDRESS, SERVER_PORT,
+                       cache)
+  
+  #################################
+  #TODO: Close socket's connection
+  #################################
+  client_sock.close()
+
+main()
